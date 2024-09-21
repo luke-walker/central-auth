4be5d1993e51a8871262b90216b30929d198f532
Luke Walker <lukeryanwalker2000@gmail.com>
2024-09-20 23:17:02 -0700

    Create DB setup scripts

---
+++ db/002_create_tables.sql
@@ -0,0 +1,29 @@
+DROP TABLE IF EXISTS servers CASCADE;
+CREATE TABLE servers (
+    id              uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+    name            text UNIQUE NOT NULL,
+    addresses       text[],
+    redirect_url    text NOT NULL,
+    token           uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+    id              uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+    token           uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+    username        text UNIQUE NOT NULL,
+    password        text NOT NULL,
+    last_ip         text,
+    PRIMARY KEY (id, token)
+);
+
+DROP TABLE IF EXISTS sessions CASCADE;
+CREATE TABLE sessions (
+    user_ip         text NOT NULL,
+    user_token      uuid NOT NULL,
+    access_token    uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+    expires         date,
+    PRIMARY KEY (user_ip, user_token),
+    FOREIGN KEY (user_token) REFERENCES users(token) ON DELETE CASCADE
+);
