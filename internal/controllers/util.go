eb1f943fd42b5c380d6efe0eeb75f3eaa486a95d
Luke Walker <lukeryanwalker2000@gmail.com>
2024-09-23 23:07:45 -0700

    Create getUserIp function
    Returns an available IP from the user

--- internal/controllers/util.go
+++ internal/controllers/util.go
@@ -4,6 +4,14 @@ import (
     "net/http"
 )
 
+func getUserIp(r *http.Request) string {
+    userIp := r.Header.Get("X-Forwarded-For")
+    if userIp == "" {
+        userIp = r.RemoteAddr
+    }
+    return userIp
+}
+
 func errInvalidRequestMethod(w http.ResponseWriter) {
     http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 }
