c7c7f20005c8e242490565690e1edca19c76184c
Luke Walker <lukeryanwalker2000@gmail.com>
2024-09-26 01:17:04 -0700

    Check for Authorization header
    Only add bearer token if Authorization header is missing

--- pkg/middleware/bearer_token_header.go
+++ pkg/middleware/bearer_token_header.go
@@ -6,12 +6,14 @@ import (
 
 func AddBearerTokenHeader(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        accessToken, err := r.Cookie("access_token")
-        if err != nil {
-            http.Error(w, "Cookie 'access_token' not found", http.StatusBadRequest)
-            return
+        if r.Header.Get("Authorization") == "" {
+            accessToken, err := r.Cookie("access_token")
+            if err != nil {
+                http.Error(w, "Cookie 'access_token' not found", http.StatusBadRequest)
+                return
+            }
+            r.Header.Add("Authorization", "Bearer " + accessToken.Value)
         }
-        r.Header.Add("Authorization", "Bearer " + accessToken.Value)
 
         next.ServeHTTP(w, r)
     })
